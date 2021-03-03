01fecb01e5446933e86a029dd917214a5df26773
Ger Hobbelt <ger@hobbelt.com>
2021-03-03 14:46:29 +0100

    removed all code duplication with the Chromely project itself. (prepwork for a full-source build of the Chromely apps)

--- src/Chromely.CefSharp/ChromelyBasicApp.cs
+++ src/Chromely.CefSharp/ChromelyBasicApp.cs
@@ -1,7 +1,7 @@
 ﻿// Copyright © 2017-2020 Chromely Projects. All rights reserved.
 // Use of this source code is governed by Chromely MIT licensed and CefSharp BSD-style license that can be found in the LICENSE file.
 
-using Chromely.CefSharp.NativeHost;
+using Chromely.NativeHost;
 using Chromely.Core.Host;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
